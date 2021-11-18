f842283affa458bf6f070cf5d60d4a9be216b0b1
v1j4y <vijay.gopal.c@gmail.com>
2021-11-18 13:20:21 +0100

    updated stuff

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,28 +2,18 @@ using BinaryBuilder
 
 # Collection of sources required to build Xsum
 sources = [
-    "https://github.com/Cyan4973/xxHash/archive/v0.7.0.tar.gz" =>
-    "b34792646d5e19964bb7bba24f06cb13aecaac623ab91a54da08aa19d3686d7e",
+    "https://github.com/v1j4y/div_by_2/releases/download/v4.0/div_by_2-5.0.tar.gz" =>
+    "c860b07e114321e10e5d9e5598ad4696",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/xxHash-0.7.0
-mkdir -p ${prefix}/lib
+cd $WORKSPACE/srcdir/div_by_2-5.0
+./autogen.sh
+./configure --prefix=${prefix}
 make
-if [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]
-then
-    cp libxxhash.so ${prefix}/lib/libxxhash.so
-else
-    cp libxxhash.${dlext} ${prefix}/lib/libxxhash.${dlext}
-fi
+make install
 """
-# if [[ "$(uname)" == 'Darwin' ]]
-# then
-#     cp libxxhash.dylib ${prefix}/lib/libxxhash.${dlext}
-# else
-#     cp libxxhash.so ${prefix}/lib/libxxhash.${dlext}
-# fi
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
@@ -31,7 +21,7 @@ platforms = supported_platforms() # build on all supported platforms
 
 # The products that we will ensure are always built
 products(prefix) = [
-    LibraryProduct(prefix, "libxxhash", :libxxhash),
+    LibraryProduct(prefix, "libdivby2", :libdivby2),
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -39,7 +29,7 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "xxhash", v"0.7", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, "div_by_2", v"5.0", sources, script, platforms, products, dependencies)
 
 println("Contents of ", pwd(), " = ", readdir("."))
 if isdir("products")
